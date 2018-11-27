445566fc23d4afd22dec8fed892df58e476aae8e
codergenius <45354320+codergenius@users.noreply.github.com>
2018-11-27 12:48:39 +0100

    Add files via upload
    Classifies numbers into grades

---
+++ grade calculator.py
@@ -0,0 +1,37 @@
+import math
+
+a = int(input("Enter a number: "))
+if (a>=70):
+    print("You got an A")
+else:
+    print("no score")
+
+if (a >= 60):
+    print("You got a B")
+else:
+    null
+          
+if (a >= 50):
+    print("You got a C")
+else:
+    null
+    
+if (a >= 45):
+    print("You got a D")
+else:
+    null
+    
+if(a >= 40):
+    print ("You got an E")
+
+
+elif (a>=20):
+    print("You Failed woefully, Try again")
+
+else:
+    print("You Failed, Try again")
+                        
+
+
+ 
+    
